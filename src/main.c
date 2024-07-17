d6fc48b4635ca2b5ef404dd657f39b6f2f641d72
8dcc <8dcc.git@gmail.com>
2024-07-17 17:25:32 +0200

    Add header and example

---
+++ src/main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+#define LOG_COLOR
+#include "log.h"
+
+static void my_func(void) {
+    log_dbg("Reached the desired point...");
+}
+
+int main(void) {
+    log_inf("Testing formats: %d", 123);
+    my_func();
+    log_wrn("Got negative value");
+    log_err("Unexpected value: %p", NULL);
+    log_ftl("Failed to allocate enough memory");
+
+    return 0;
+}

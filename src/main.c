853261dbcda32a285de64a75dbd16f9fe2bdb8e3
8dcc <8dcc.git@gmail.com>
2025-02-22 16:03:24 +0100

    Check values returned by 'fopen' and 'log_add_file' in 'main.c'

--- src/main.c
+++ src/main.c
@@ -9,9 +9,16 @@ static void my_func(void) {
 int main(void) {
     FILE* err_file = fopen("error.log", "w");
     FILE* dbg_file = fopen("debug.log", "w");
-    log_add_file(stderr, LOG_TAG_ALL);
-    log_add_file(err_file, LOG_TAG_AND_ABOVE(LOG_TAG_ERR));
-    log_add_file(dbg_file, LOG_TAG_AND_BELOW(LOG_TAG_INF));
+    if (!err_file || !dbg_file)
+        return 1;
+
+    if (!log_add_file(stderr, LOG_TAG_ALL) ||
+        !log_add_file(err_file, LOG_TAG_AND_ABOVE(LOG_TAG_ERR)) ||
+        !log_add_file(dbg_file, LOG_TAG_AND_BELOW(LOG_TAG_INF))) {
+        fclose(err_file);
+        fclose(dbg_file);
+        return 1;
+    }
 
     log_inf("Testing formats: %d", 123);
     my_func();
